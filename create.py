8aa73c87114bed25e3188c6ac04dfc6ccd586543
YannickBezes <yannick.bezes@gmail.com>
2019-06-02 14:04:45 +0200

    use config file instead of username and password written directly in the file

--- create.py
+++ create.py
@@ -1,18 +1,14 @@
 import sys
 import os
 from github import Github
-
-path = "/Users/kalle/Documents/Projects/MyProjects/"
-
-username = "" #Insert your github username here
-password = "" #Insert your github password here
+from config import *
 
 def create():
-    folderName = str(sys.argv[1])
-    os.makedirs(path + str(sys.argv[1]))
-    user = Github(username, password).get_user()
-    repo = user.create_repo(sys.argv[1])
-    print("Succesfully created repository {}".format(sys.argv[1]))
+    repo_name = str(sys.argv[1])
+    os.makedirs(PATH + repo_name)
+    user = Github(USERNAME, PASSWORD).get_user()
+    repo = user.create_repo(repo_name)
+    print("Succesfully created repository {}".format(repo_name))
 
 if __name__ == "__main__":
     create()
